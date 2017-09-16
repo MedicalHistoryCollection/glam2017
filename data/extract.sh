d992fc9921e22edb2ef41497017c994479547486
bellisk <daisywheel22@gmail.com>
2017-09-16 16:30:08 +0200

    Rearrange data. pictures_per_category would go into data/, but because they are large, not committing them to git.

--- data/extract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-input="categories.txt"
-while IFS= read -r var
-do
-  echo "Category: $var"
-  grep ",$var"  data_library_small_02.csv >books_$var.csv
-  grep ",$var," pictures_database.csv     >photos_$var.csv
-done < "$input"
-
