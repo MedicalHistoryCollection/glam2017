b1e88cb78c434ceaabb7a5775eacc0ab00a8bbf1
bellisk <daisywheel22@gmail.com>
2017-09-16 17:17:25 +0200

    Get photo links in html

--- scripts/render_html.py
+++ scripts/render_html.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 import csv, os.path
+import hashlib
 
 env = Environment(
     loader=PackageLoader('scripts', 'templates'),
@@ -10,6 +11,21 @@ template = env.get_template('carousel_template.html')
 data_dir = 'data/'
 
 categories = {}
+
+
+def get_wikimedia_link(row):
+    f = '.'.join(row['Image'].strip('0').split('.')[0:-1]) + '.jpg'
+    filename = row['Title'].replace(' ', '_') + '_-_' + f
+
+    m = hashlib.md5()
+    m.update(filename.encode('utf-8'))
+    md5 = m.hexdigest()
+
+    base_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/'
+    subdirs = md5[0] + '/' + md5[0:2] + '/'
+    size = '/800px-'
+
+    return base_url + subdirs + filename + size + filename
 
 with open(os.path.join(data_dir, 'categories.txt')) as f:
     for line in f:
@@ -28,8 +44,6 @@ for cat in categories.keys():
     categories[cat] = photos
 
 photo_metadata = os.path.join(data_dir, 'pictures_database_existing.csv')
-with open(photo_metadata) as f:
-    reader = csv.DictReader(f)
 
 # One html page per category
 for cat in categories.keys():
@@ -58,19 +72,19 @@ for cat in categories.keys():
                     object['rank'] = n
                     object['title'] = row['Title']
                     object['description'] = row['Description']
-                    object['image_link'] = '' # Todo: get this from wikimedia
+                    object['image_link'] = get_wikimedia_link(row)
                     object['books'] = []
 
                     n += 1
 
                     context['objects'].append(object)
 
-    with open(context['name'] + '.html', 'w') as g:
+    with open(os.path.join('html', context['name'] + '.html'), 'w') as g:
         print('Rendering template for category', context['name'])
 
         g.write(template.render(context))
 
-    break
+print('Done')
 
 
 
