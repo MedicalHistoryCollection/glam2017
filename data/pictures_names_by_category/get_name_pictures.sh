c3078b43c4796b5164d7e1affebb22e600cfdab7
Arturo Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2017-09-16 10:04:03 +0200

    correct name of pictures by categories

--- data/pictures_names_by_category/get_name_pictures.sh
+++ data/pictures_names_by_category/get_name_pictures.sh
@@ -3,6 +3,6 @@ input="categories.txt"
 while IFS= read -r var
 do
   echo "Category: $var"
-  awk 'BEGIN {FS = ","}; {print $2}' books_$var.csv >name_picutes_files_category_$var.csv
+  awk 'BEGIN {FS = ","}; {print $2}' photos_$var.csv >name_picutes_files_category_$var.csv
 done < "$input"
 
