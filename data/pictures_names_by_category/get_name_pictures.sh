5d2125e7f4c1d5ffe316ba68d927594ce5a524b3
Arturo Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2017-09-16 10:01:59 +0200

    Adding pictures names separated by categories

---
+++ data/pictures_names_by_category/get_name_pictures.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+input="categories.txt"
+while IFS= read -r var
+do
+  echo "Category: $var"
+  awk 'BEGIN {FS = ","}; {print $2}' books_$var.csv >name_picutes_files_category_$var.csv
+done < "$input"
+
